24154331f1cab6322dfc83ed4d9ee46e28f25a71
TranTam <tamvaa1306@gmail.com>
2022-09-17 22:02:28 +0700

    built-in generics

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // Code
+const names = ["Max", "Manu"];
+// names[0].split(" ");
+console.log(names[0].split(" "));
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("This is done!");
+    }, 2000);
+});
+promise.then((data) => {
+    data.split(" ");
+});
