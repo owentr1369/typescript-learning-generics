ef72fec71c7e7132c809e972fbeb1e898d3785c8
TranTam <tamvaa1306@gmail.com>
2022-09-17 22:24:03 +0700

    creating a generic function

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,18 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Code
-const names = ["Max", "Manu"];
-// names[0].split(" ");
-console.log(names[0].split(" "));
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("This is done!");
-    }, 2000);
-});
-promise.then((data) => {
-    data.split(" ");
-});
+// const names: Array<string> = ["Max", "Manu"];
+// // names[0].split(" ");
+// // console.log(names[0].split(" "));
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
+// promise.then((data) => {
+//   data.split(" ");
+// });
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergedObj.age);
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,24 @@
-import { resolveModuleName } from "typescript";
-
 // Code
-const names: Array<string> = ["Max", "Manu"];
+// const names: Array<string> = ["Max", "Manu"];
 
-// names[0].split(" ");
+// // names[0].split(" ");
 
-console.log(names[0].split(" "));
+// // console.log(names[0].split(" "));
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("This is done!");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+
+console.log(mergedObj.age);
